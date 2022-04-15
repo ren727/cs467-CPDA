aba2023897d1e53c44ba3a6b0a53b0af0a010859
Casey Arndt <ca895534@gmail.com>
2022-04-15 17:26:37 -0400

    first commit

---
+++ resources/comments.py
@@ -0,0 +1,14 @@
+def get_comments(request):
+    pass
+
+def post_comments(request):
+    pass
+
+def get_comment(request, id):
+    pass
+
+def put_comment(request, id):
+    pass
+
+def delete_comment(request, id):
+    pass
