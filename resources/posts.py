9ea12f1bb4750ac3675a0e7e33ae9aaa7e10497a
zhenjing <zhenjing@engr.orst.edu>
2022-04-28 11:44:03 -0700

    add the edit function and remove attach comment to post from this file

--- resources/posts.py
+++ resources/posts.py
@@ -1,7 +1,53 @@
 from google.cloud import datastore
+from datetime import datetime
+from pytz import timezone
 
 # establish the connection
 client = datastore.Client()
+'''
+entity - posts({
+        "user_id": post_data['user_id'],
+        "title": post_data['title'],
+        "content": post_data['content'],
+        "categories": post_data['categories'],
+        "created_at": time,
+        "comments": [],
+        "modified_at": None,
+    })
+'''
+
+
+def post_exists(post_id):
+    post_key = client.key("posts", int(post_id))
+    post_found = client.get(key=post_key)
+
+    if not post_found:
+        msg = {
+            "Error": "No post with this id exists."
+        }
+        return False, msg
+    else:
+        return True, ''
+
+
+def validate_post(post_data):
+    # check the required attributes
+    required_attri = ['user_id', 'title', 'content', 'categories']
+    if any(attr not in post_data for attr in required_attri):
+        msg = {
+            'Error': "Should include all the required attributes."
+        }
+    # check the length of the content
+    # should be in range [1, 255]
+    if len(post_data['content']) == 0:
+        msg = {
+            'Error': 'The post can not be empty!'
+        }
+    elif len(post_data['content']) > 255:
+        msg = {
+            'Error': 'The post is longer than 255 characters!'
+        }
+    return False, msg if msg else True, None
 
 
 def fetch_posts(base_url, q_limit, q_offset):
@@ -26,19 +72,12 @@ def fetch_posts(base_url, q_limit, q_offset):
     return output
 
 
-def store_new_post(post_data):
-    if len(post_data) == 0:
-        msg = {
-            'Error': 'The post can not be empty!'
-        }
-    elif len(post_data) > 255:
-        msg = {
-            'Error': 'The post is longer than 255 characters!'
-        }
-    entity = datastore.Entity(key=client.key('posts'))
+def store_new_post(base_url, post_data):
+    new_post = datastore.entity.Entity(key=client.key('posts'))
     # time is set by the system
-    time = a_time_function_here
-    entity.update({
+    pst_timezone = timezone('US/Pacific')
+    time = datetime.now(pst_timezone)
+    new_post.update({
         "user_id": post_data['user_id'],
         "title": post_data['title'],
         "content": post_data['content'],
@@ -47,30 +86,27 @@ def store_new_post(post_data):
         "comments": [],
         "modified_at": None,
     })
-    client.put(entity)
-    msg = {
-        'Sucess': 'Post was added to the database.'
+    client.put(new_post)
+    result = {
+        "id": str(new_post.key.id),
+        "user_id": post_data['user_id'],
+        "title": post_data['title'],
+        "content": post_data['content'],
+        "categories": post_data['categories'],
+        "created_at": time,
+        "comments": [],
+        "modified_at": None,
+        "self": base_url + '/' + str(new_post.key.id),
+
     }
-    return msg
+    return result
 
 
-def fetch_data(entity, id):
-    data_key = client.key(entity, int(id))
-    data_found = client.get(key=data_key)
-    return data_found
-
-
-def fetch_single_post(post_id, self_url):
-    post_found = fetch_data('posts', post_id)
-    if post_found:
-        post_found['id'] = str(id)
-        post_found['self'] = self_url
-        return post_found
-    else:
-        msg = {
-            "Error": "No post with this id exists"
-        }
-        return msg
+def fetch_single_post(post_id):
+    post_key = client.key('posts', int(post_id))
+    post_found = client.get(key=post_key)
+    post_found['id'] = str(id)
+    return post_found
 
 
 def delete_single_post(post_id):
@@ -82,88 +118,65 @@ def delete_single_post(post_id):
     return msg
 
 
-def get_comments_for_post(post_id):
-    post_found = fetch_data('posts', post_id)
-    if not post_found:
-        msg = {
-            "Error": "No post with this id exists."
-        }
-        return msg
-
-    comments_for_post = []
-    if post_found['commments']:
-        for comment_id in post_found['comments']:
-            comment_found = fetch_data("comments", comment_id)
-            comments_for_post.append(comment_found)
-        return comments_for_post
-    else:
-        return []
-
-
-def validate_post_and_comment(post_id, comment_id):
-    post_found = fetch_data('posts', post_id)
-    comment_found = fetch_data('comments', comment_id)
-    if not post_found and not comment_found:
-        msg = {
-            "Error": "Both post and comment are not found."
-        }
-    elif not post_found:
-        msg = {
-            "Error": "No post with this id exists."
-        }
-    elif not comment_found:
-        msg = {
-            "Error": "No post with this id exists."
-        }
-    else:
-        msg = {
-            "Found": "Both post and comment exist."
-        }
-
-    return msg
-
-
-def add_comment_to_post(post_id, comment_id):
-    post_found = fetch_data('posts', post_id)
-    comment_found = fetch_data('comments', comment_id)
-    if comment_found['post']:
-        if comment_found['post'] == post_id:
-            msg = {
-                "Error": "This comment is already attached to this post."
-            }
-        else:
-            msg = {
-                "Error": "This comment had been attached to another post."
-            }
-    else:
-        comment_found['post'] = post_id
-        client.put(comment_found)
-
-    if post_found['comments']:
-        post_found['comments'].append(comment_id)
-    else:
-        post_found['comments'] = [comment_id]
+def edit_post(post_id, edit_data):
+    post_found = fetch_single_post(post_id)
+    for key, val in edit_data:
+        post_found[key] = val
     client.put(post_found)
-    msg = {
-        'Sucess': 'Comment was attached to the post.'
-    }
-    return msg
+    return post_found
+
+# def get_comments_for_post(post_id):
+#     post_found = fetch_single_post(post_id)
+#     comments_for_post = []
+#     if len(post_found['commments']) != 0:
+#         for comment_id in post_found['comments']:
+#             comment_found = fetch_comment(comment_id)
+#             comments_for_post.append(comment_found)
+#         return comments_for_post
+#     else:
+#         return []
 
 
-def remove_comment_from_post(post_id, comment_id):
-    post_found = fetch_data('posts', post_id)
-    comment_found = fetch_data('comments', comment_id)
-    if post_found['comments']:
-        if comment_id in post_found['comments']:
-            post_found['comments'].remove(comment_id)
-            comment_found['post'] = None
-            client.put(post_found)
-            client.put(comment_found)
-            msg = {
-                'Sucess': 'Comment was attached to the post.'
-            }
-        else:
-            msg = {
-                'Error': "This comment is not for this post."
-            }
-    return msg
+# def add_comment_to_post(post_id, comment_id):
+#     post_found = fetch_single_post(post_id)
+#     comment_found = fetch_comment(comment_id)
+#     if comment_found['post_id']:
+#         if comment_found['post_id'] == post_id:
+#             msg = {
+#                 "Error": "This comment is already attached to this post."
+#             }
+#         else:
+#             msg = {
+#                 "Error": "This comment had been attached to another post."
+#             }
+#     else:
+#         # update comment
+#         comment_found['post'] = post_id
+#         client.put(comment_found)
+#         # update post
+#         post_found['comments'].append(comment_id)
+#         client.put(post_found)
+#     msg = {
+#         'Sucess': 'Comment was attached to the post.'
+#     }
+#     return msg
+
+
+# def remove_comment_from_post(post_id, comment_id):
+#     post_found = fetch_single_post(post_id)
+#     comment_found = fetch_comment(comment_id)
+#     if comment_id in post_found['comments']:
+#         # update post
+#         post_found['comments'].remove(comment_id)
+#         client.put(post_found)
+#         # update comment
+#         comment_found['post_id'] = None
+#         client.put(comment_found)
+#         msg = {
+#             'Sucess': 'Comment was attached to the post.'
+#         }
+#     else:
+#         msg = {
+#             'Error': "This comment is not for this post."
+#         }
+#     return msg
